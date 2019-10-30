0d216ab51661cc746d97e62907bc17ff5d7d348d
yang.lan <496713192@qq.com>
2019-10-30 22:56:20 +0800

    添加文件导入与下载模板

--- src/views/student/list.vue
+++ src/views/student/list.vue
@@ -1,19 +1,75 @@
 <template>
-<div>
-
-</div>
+    <div>
+        <el-button type="primary"  @click="dialogFormVisible = true">导入数据</el-button>
+        <el-dialog title="导入数据" :visible.sync="dialogFormVisible">
+            <el-button type="success" ><el-link href="https://element.eleme.io" target="_blank" :underline="false">下载模板</el-link></el-button>
+            <el-upload
+                class="upload-demo"
+                action="https://jsonplaceholder.typicode.com/posts/"
+                :on-change="handleChange"
+                :file-list="fileList">
+                <el-button type="primary">点击上传</el-button>
+                <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+            </el-upload>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        </div>
+        </el-dialog>
+    </div>
 </template>
 
 <script>
-export default {
-    data(){
-        return {
-
-        }
+  export default {
+    data() {
+      return {
+        gridData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }],
+        dialogTableVisible: false,
+        dialogFormVisible: false,
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '120px',
+        fileList:[]
+        // fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+      }
     },
-}
+    methods: {
+      handleChange(file, fileList) {
+        this.fileList = fileList.slice(-3);
+      }
+    }
+  }
 </script>
 
-<style>
-
+<style scoped>
+    a{
+        color:white;
+    }
+    .el-link.el-link--default {
+        color: white;
+}
 </style>
